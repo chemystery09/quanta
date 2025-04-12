de462adff41fd4e31c2a54b042ff338fcefe981b
Che <michelle.che05@gmail.com>
2025-04-12 17:12:49 -0400

    poop

--- gui/app/routes/home.tsx
+++ gui/app/routes/home.tsx
@@ -1,103 +1,15 @@
 import type { Route } from "./+types/home";
 import { useState } from "react";
+import Landing from "./landing";
+import Real from "./real";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "CT goods" },
+    { title: "QNanoFEA" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  const [section, setSection] = useState("dashboard");
-  const refreshPage = () => {
-    window.location.reload();
-  };
-  const closeWindow = () => {
-    window.close();
-  };
-  
-  return <div className = "everything">
-     <nav className = "nav"> 
-        <div className ="title"> CT Goods !! </div>
-        <div className ="subtitle"> Check out CT's &lt;3 </div>
-    </nav>
-
-    <main className = "mainbody">
-        <div className="sidebar">
-            <div className="topButtons">
-              <button className ="mainB" id="dasboardButton" onClick={() => setSection('dashboard')}>
-                Search Patients
-              </button>
-              <button className ="mainB" id="systemsButton" onClick={() => setSection('systems')}>
-                Search Patients
-              </button>
-              <button className ="mainB" id="guideButton" onClick={() => setSection('guide')}>
-                Render
-              </button>
-              <button className ="setB" id="settingsButton" onClick={() => setSection('settings')}>
-                Upload CT Scans
-              </button>
-            </div>
-            
-            <div className="bottomButtons">
-              <button className ="logB" id="logoutButton" onClick={() => closeWindow()}>
-                Logout
-              </button>
-            </div>
-            
-            
-        </div>
-
-        <div className = "sidedisplay">
-            {
-              section === 'dashboard' && (
-                <div id="dashboard">
-                  <div className="main-flex">
-                    <button className = "refresh" onClick={() => refreshPage()}>Refresh</button>
-                  </div>
-
-                  <div className="update-text">
-                  </div>
-                </div>
-              )
-            }
-
-            {
-              section === 'systems' && (
-                <div id="systems">
-                  <div className = "main-flex">
-                      <input className="searchContentBar" type="text" id="searchFiles"
-                          placeholder="Search Books..."/> 
-                      <button className = "refresh" onClick={() => refreshPage()}>Refresh</button>
-                  </div>
-                </div>
-              )
-            }
-
-            {
-            section === 'guide' && (
-                <div id="guide">
-                <h1>Guide</h1>
-                    
-                </div>
-                )
-              }
-
-            
-            {
-              section === 'settings' && (
-                <div id="settings">
-                   
-                </div>
-              )
-            }
-        </div>
-
-    </main>
- 
-
-
-
-  </div>;
+  return <Landing />;
 }
