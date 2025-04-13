59cd8c76d26dd174db32aee68bf4762b0bfb3211
chemystery09 <82279192+chemystery09@users.noreply.github.com>
2025-04-13 09:17:58 -0400

    added quantum to flask

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 from flask import Flask, request
 from fea_classical import load_mesh, get_material, generate_mesh_stiffness, build_square_sheet, get_new_nodes
+from vqls import generate_grid_stiffness, transform_matrix_to_unit_interval, server_setup, run
 import numpy as np
 import uuid
 import os
+import classiq
+from classiq import (
+    QuantumProgram,
+    Output,
+    create_model,
+    power,
+    prepare_amplitudes,
+    synthesize,
+    unitary,
+    execute,
+    show,
+    CArray,
+    CReal,
+    QArray,
+    QBit,
+    QNum,
+    qfunc,
+)
+from classiq.qmod.symbolic import floor, log
+from classiq import *
+import scipy
+
 
 app = Flask(__name__)
 
@@ -21,6 +44,67 @@ def upload():
         'message': 'File uploaded successfully',
         'file_path': obj_file_path
     }
+
+@app.route('api/v1/simulate_quantum', methods=['POST'])
+def simulate_quantum():
+    obj_file_path = request.json.get('file_path')
+    if not obj_file_path:
+        n = request.json.get('n', 32)
+
+        if not n:
+            return {'error': 'Grid size not specified'}, 400
+        
+        elements, nodes = build_square_sheet(n)
+    else:
+        # Check if the file exists
+        if not obj_file_path or not os.path.exists(obj_file_path):
+            return {'error': 'File not found'}, 404
+        
+        obj_file_path = os.path.abspath(obj_file_path)
+        if not obj_file_path.startswith(os.path.abspath('uploads/')):
+            return {'error': 'Invalid file path'}, 400
+    data = request.get_json
+    n = data.get('n', 4)
+    material_id = request.json.get('material_id')
+    if not material_id:
+        return {'error': 'Material ID not specified'}, 400
+    bulk_modulus, shear_modulus = get_material(material_id)
+    K, f = generate_grid_stiffness(n, bulk_modulus, shear_modulus)
+    server_setup()
+    A, info = transform_matrix_to_unit_interval(A)
+    b = f / np.linalg.norm(f)
+    m = 32  # Precision of a binary representation, e.g. 32 binary digits
+    sign = lambda num: "-" if num < 0 else ""  # Calculate sign of a number
+    fixed_nodes = request.json.get('fixed_nodes', [])
+    if not fixed_nodes:
+        return {'error': 'Fixed nodes not specified'}, 400
+
+    forces = request.json.get('forces', [])
+    if not forces:
+        return {'error': 'Forces not specified'}, 400
+
+    binary = lambda fraction: str(
+        np.binary_repr(int(np.abs(fraction) * 2 ** (m))).zfill(m)
+    ).rstrip(
+        "0"
+    )  # Binary representation of a fraction
+    amplitudes = b.tolist()
+    # Parameters for the QPE
+    precision = 4
+    u_quantum = run()
+    return {
+        'original_nodes': nodes.tolist(),
+        'elements': elements.tolist(),
+        'nodes': get_new_nodes(u_quantum, nodes).tolist(),
+        'forces': f.tolist(),
+        'displacement': u_quantum.tolist(),
+        'bulk_modulus': bulk_modulus,
+        'shear_modulus': shear_modulus,
+        'material_id': material_id
+    }
+
+
+
 
 @app.route('/api/v1/simulate', methods=['POST'])
 def simulate():
