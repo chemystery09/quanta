cf6c4b691e522ee4eb66f59898cd5ec5e6fa5636
Jessica Su <aes6qb@virginia.edu>
2025-04-13 02:52:24 -0400

    idk

--- gui/app/routes/real.tsx
+++ gui/app/routes/real.tsx
@@ -3,10 +3,24 @@ import { useState } from "react";
 import "./css/real.css";
 import { Link } from "react-router-dom";
 import type { Route } from "./+types/home";
+import {
+  type FileUpload,
+  parseFormData,
+} from "@mjackson/form-data-parser";
+import type { ActionFunctionArgs } from "react-router";
 
 
 export default function Real() {
-    const [section, setSection] = useState("dashboard");
+    const uploadHandler = async (fileUpload: FileUpload) => {
+        if (fileUpload.fieldName === "avatar") {
+          // process the upload and return a File
+        }
+    
+      console.log(fileUpload);
+    }
+
+    const [section, setSection] = useState("settings");
+
     const refreshPage = () => {
       window.location.reload();
     };
@@ -23,18 +37,15 @@ export default function Real() {
       <main className = "mainbody">
           <div className="sidebar bg-teal-50 shadow-xl">
               <div className="topButtons">
+              <button className ="setB bg-teal-500" id="settingsButton" onClick={() => setSection('settings')}>
+                  Simulate Models
+                </button>
+
                 <button className ="mainB bg-teal-500" id="dasboardButton" onClick={() => setSection('dashboard')}>
                   Guide
                 </button>
-                <button className ="mainB bg-teal-500" id="systemsButton" onClick={() => setSection('systems')}>
-                  Search Materials
-                </button>
-                <button className ="mainB bg-teal-500" id="guideButton" onClick={() => setSection('guide')}>
-                  Upload OBJ File
-                </button>
-                <button className ="setB bg-teal-500" id="settingsButton" onClick={() => setSection('settings')}>
-                  Simulate
-                </button>
+             
+                
               </div>
               
               
@@ -47,40 +58,28 @@ export default function Real() {
                     <div className="main-flex">
                       <button className = "refresh bg-teal-500" onClick={() => refreshPage()}>Refresh</button>
                     </div>
-  
+                    
                     <div className="update-text">
                     </div>
                   </div>
+
                 )
               }
   
-              {
-                section === 'systems' && (
-                  <div id="systems">
-                    <div className = "main-flex">
-                        <input className="searchContentBar" type="text" id="searchFiles"
-                            placeholder="Search Materials..."/> 
-                        <button className = "refresh" onClick={() => refreshPage()}>Refresh</button>
-                    </div>
-                  </div>
-                )
-              }
-  
-              {
-              section === 'guide' && (
-                  <div id="guide">
-                  <h1>Guide</h1>
-                      
-                  </div>
-                  )
-                }
+              
   
               
               {
                 section === 'settings' && (
-                  <div id="settings">
-                     <Link to="/three" className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-purple-300 hover:text-white">
-                        Simulate Model
+                  <div className="sub-flex w-full items-center justify-between gap-4">
+                    <input className="searchContentBar" type="text" id="searchFiles"
+                            placeholder="Search Materials..."/> 
+                    
+                    <input type = "file" className ="bg-teal-500 hidden" id="guideButton" onChange={(event) => uploadHandler(event)} />
+                      <label htmlFor="guideButton" className="uploadB bg-teal-100 rounded-md px-10 py-2">Upload</label>
+
+                     <Link to="/three" className="simulate rounded-md bg-teal-500 px-10 py-2 hover:bg-teal-300 hover:text-white">
+                        Simulate
                      </Link>
                   </div>
                 )
